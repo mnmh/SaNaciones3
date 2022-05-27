526bd7da555a25924c28bf20f39285a6f41c410a
alrodriguezdg <alrodriguezdg@gmail.com>
2022-05-27 08:57:09 -0500

    Avances parallax

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+/*** PARALLAX ***/
+
+const tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#contentParallax",
+		start: "top top",
+		end: "bottom top",
+		scrub: true
+	}
+});
+
+gsap.utils.toArray(".parallax").forEach(layer => {
+	const depth = layer.dataset.depth;
+	const movement = -(layer.offsetHeight * depth)
+	tl.to(layer, {y: movement, ease: "none"}, 0)
+});
